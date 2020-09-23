1652630a557e391f32dda00d6d5ae76cff45270d
Raf Millan <rafmillan@Rafs-MacBook-Pro.local>
2020-09-23 14:37:33 -0400

    try

--- app.py
+++ app.py
@@ -11,21 +11,22 @@ from flask import Flask, render_template, url_for, request, redirect, session
 import time
 
 app = Flask(__name__)
-key = str(time.time())
-app.secret_key = key
+
+app.secret_key = 'key'
 
 API_BASE = 'https://accounts.spotify.com'
+
 clientID ="4b3a1682d17c4711b3943fdc7eec3cde" 
 secretID = "7f3bcf6e081846b88b6dcf3a658203a5"
 
+# Make sure you add this to Redirect URIs in the setting of the application dashboard
+REDIRECT_URI = "http://127.0.0.1:5000/api_callback"
+#REDIRECT_URI = "https://spotmytracks.herokuapp.com/api_callback"
+
 os.environ['SPOTIPY_CLIENT_ID']= clientID
 os.environ['SPOTIPY_CLIENT_SECRET']= secretID
-#os.environ['SPOTIPY_REDIRECT_URI']='http://127.0.0.1:5000/api_callback'
-os.environ['SPOTIPY_REDIRECT_URI']='https://spotmytracks.herokuapp.com/api_callback'
+os.environ['SPOTIPY_REDIRECT_URI']= REDIRECT_URI
 
-# Make sure you add this to Redirect URIs in the setting of the application dashboard
-#REDIRECT_URI = "http://127.0.0.1:5000/api_callback"
-REDIRECT_URI = "https://spotmytracks.herokuapp.com/api_callback"
 SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read'
 
 # Set this to True for testing but you probaly want it set to False in production.
@@ -63,7 +64,7 @@ def api_callback():
 def index():
     print("/index")
     username = ""
-    token = util.prompt_for_user_token(username, SCOPE)
+    #token = util.prompt_for_user_token(username, SCOPE)
     list_of_results = []
     list_of_artist_names = []
     list_of_artist_uri = []
@@ -76,7 +77,12 @@ def index():
     list_of_artwork = []
     list_of_release_dates = []
 
-    sp = spotipy.Spotify(auth=token)
+    session['token_info'], authorized = get_token(session)
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+    data = request.form
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
     results = sp.current_user_top_tracks(limit=10,offset=0,time_range='medium_term')
     user = sp.current_user()
     userList = []
@@ -154,8 +160,31 @@ def index():
     all_songs_saved = all_songs_meta.to_csv('top10_songs.csv')
     
     return render_template("index.html", column_names=top_songs_pretty.columns.values, row_data=top_songs_pretty.values.tolist(),
-                           link_column="Patient ID", zip=zip)
-    #return render_template("index.html")
+                           link_column="", zip=zip)
+
+# Checks to see if token is valid and gets a new token if not
+def get_token(session):
+    print('getToken()')
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
+        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = clientID, client_secret = secretID, redirect_uri = REDIRECT_URI, scope = SCOPE)
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 
 
